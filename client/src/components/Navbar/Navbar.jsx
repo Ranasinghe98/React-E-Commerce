543745faaf5af4e94e6cf0d9bb38fb3a95b8a95b
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2022-12-29 23:59:38 +0530

    update

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDownIcon'
+import SearchIcon from '@mui/icons-material/SearchIcon'
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlinedIcon'
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlinedIcon'
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlinedIcon'
 
 const Navbar = () => {
     return(<>
-        <h1>This is nav bar</h1>
+        <div className='navbar'>
+            <div className='wrapper'>
+                <div className='left'>
+                    <div className='item'>
+                        <img src='/images/en.png' alt='flag' />
+                        <KeyboardArrowDownIcon />
+                    </div>
+                </div>
+                <div className='center'></div>
+                <div className='right'></div>
+            </div>
+        </div>
     </>)
 }
 
