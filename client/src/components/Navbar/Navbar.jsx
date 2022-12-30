81c8c859a9bb15cb3759bd484ae2fc2305991a1c
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2022-12-31 00:14:14 +0530

    navbar update

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -4,6 +4,8 @@ import SearchIcon from '@mui/icons-material/Search'
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined'
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
+import { Link } from 'react-router-dom';
+import ShoppingCartOutlined from '@mui/icons-material/ShoppingCartOutlined';
 
 const Navbar = () => {
     return(<>
@@ -14,9 +16,46 @@ const Navbar = () => {
                         <img src='/images/en.png' alt='flag' />
                         <KeyboardArrowDownIcon />
                     </div>
+                    <div className='item'>
+                        <span>USD</span>
+                        <KeyboardArrowDownIcon />
+                    </div>
+                    <div className='item'>
+                        <Link to="/productions/1">Women</Link>
+                    </div>
+                    <div className='item'>
+                        <Link to="/productions/2">Men</Link>
+                    </div>
+                    <div className='item'>
+                        <Link to="/productions/3">Children</Link>
+                    </div>
                 </div>
-                <div className='center'></div>
-                <div className='right'></div>
+                <div className='center'>
+                    <Link to='/'>R.Dev-Shop</Link>
+                </div>
+                <div className='right'>
+                    <div className='center'>
+                        <Link to='/'>Homepage</Link>
+                    </div>
+                    <div className='center'>
+                        <Link to='/'>About</Link>
+                    </div>
+                    <div className='center'>
+                        <Link to='/'>Contact</Link>
+                    </div>
+                    <div className='center'>
+                        <Link to='/'>Store</Link>
+                    </div>
+                    <div className='icons'>
+                        <SearchIcon/>
+                        <PersonOutlineOutlinedIcon/>
+                        <FavoriteBorderOutlinedIcon/>
+                        <div className='cartIcon'>
+                            <ShoppingCartOutlinedIcon/>
+                            <span>0</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </>)
