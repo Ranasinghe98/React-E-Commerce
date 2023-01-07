8ebc0696ad53d02af5e2b87a1ebafb54e7bb95ca
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2023-01-07 20:57:01 +0530

    path update

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const router = createBrowserRouter([
     element: <Layout/>,
     children: [
       {
-        path: '/',
+        path: '/REACT_E_COMMERCE',
         element: <Home/>
       },
       {
